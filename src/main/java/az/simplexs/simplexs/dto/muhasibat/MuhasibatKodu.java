fc42b9f053e4d6a7121d6ebbc3f3b841d70111ac
kamalniyazov032 <55350100+kamalniyazov032@users.noreply.github.com>
2026-08-24 16:49:06 +0400

    Kamal
    Deyishiklik

--- src/main/java/az/simplexs/simplexs/dto/muhasibat/MuhasibatKodu.java
+++ src/main/java/az/simplexs/simplexs/dto/muhasibat/MuhasibatKodu.java
@@ -1,3 +1,3 @@
 package az.simplexs.simplexs.dto.muhasibat;
 
-public record MuhasibatKodu(Long id, String tipKodu, String tipAdi, String ad, String aciqlama, Boolean aktiv) {}
+public record MuhasibatKodu(Long id, Long tipId, String tipKodu, String tipAdi, String ad, String aciqlama, Boolean aktiv) {}
